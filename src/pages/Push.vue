7d5c84bc4f3e6b1f69ca3a07a3e70dcdb4ff07f0
Tran Vu Linh <linhtranvu@gmail.com>
2020-11-27 14:55:10 +0700

    dsds

--- src/pages/Push.vue
+++ src/pages/Push.vue
@@ -8,12 +8,30 @@
               Device Token is used for Node-RED to known which device to send
               notification to. App Admin has feature to quickly create nodes for
               create push from Node-RED
-              <!-- <q-btn
-                color="blue"
-                @click="isHidden = !isHidden"
-                label="More Info"
-              />
-              <div v-if="!isHidden"></div> -->
+              <span>
+                <q-btn
+                  color="blue"
+                  @click="isHidden = !isHidden"
+                  label="More Info"
+              /></span>
+              <div v-if="!isHidden">
+                <div v-if="platformID === 'ios'">
+                  <span class="wrap"
+                    ><b>Debug:</b>If this data is blank, you cannot send push.
+                    APNs is: {{ apnToken }}</span
+                  >
+                  <q-btn
+                    color="orange"
+                    icon="save"
+                    @click="getApnToken"
+                    label="Get APN"
+                  />
+                </div>
+                <div v-else>
+                  Android is simple, just send message to Device Token and you
+                  get push
+                </div>
+              </div>
             </q-card-section>
           </q-card>
           <br />
@@ -25,25 +43,6 @@
             label="Device Token"
           >
           </q-input>
-          <q-input
-            v-model="apnToken"
-            outlined
-            type="textarea"
-            label="APN Token"
-          >
-          </q-input>
-          <q-btn
-            color="orange"
-            icon="save"
-            @click="getApnToken"
-            label="Get APN"
-          />
-          <!-- <div
-            class="q-pa-md q-gutter-sm items-center content-center justify-center text-left"
-          >
-            <q-btn color="green" icon="reply" to="/" label="Back" />
-            <q-btn color="orange" icon="save" @click="save" label="Save" />
-          </div> -->
         </q-form>
         <iframe
           style="border:none;"
@@ -60,82 +59,18 @@
 import axios from "axios";
 import Vue from "vue";
 
-function onDeviceReady() {
-  FirebasePlugin.onTokenRefresh(
-    function(token) {
-      // alert("Token refreshed: " + token);
-    },
-    function(error) {
-      // alert("Failed to refresh token" + error);
-    }
-  );
-
-  checkNotificationPermission(false); // Check permission then get token
-
-  // checkAutoInit()
-  // isAnalyticsCollectionEnabled()
-  // isPerformanceCollectionEnabled()
-  // isCrashlyticsCollectionEnabled()
-  // isUserSignedIn()
-
-  // Platform-specific
-
-  // if (cordova.platformId === "android") {
-  // initAndroid();
-  // } else if (cordova.platformId === "ios") {
-  initIos();
-  // }
-}
-
-document.addEventListener("deviceready", onDeviceReady, false);
-
-var initIos = function() {
-  FirebasePlugin.onApnsTokenReceived(
-    function(token) {
-      alert("APNS token received: " + token);
-    },
-    function(error) {
-      // alert("Failed to receive APNS token" + error);
-    }
-  );
-};
-
-// Notifications
-var checkNotificationPermission = function(requested) {
-  FirebasePlugin.hasPermission(function(hasPermission) {
-    if (hasPermission) {
-      // alert("Remote notifications permission granted");
-      // Granted
-      getToken();
-    } else if (!requested) {
-      // Request permission
-      // alert("Requesting remote notifications permission");
-      FirebasePlugin.grantPermission(
-        checkNotificationPermission.bind(this, true)
-      );
-    } else {
-      // Denied
-      // alert("Notifications won't be shown as permission is denied");
-    }
-  });
-};
-
-// GetToken
-var getToken = function() {
-  FirebasePlugin.getToken(
-    function(token) {
-      // alert('Got FCM token: ' + token)
-    },
-    function(error) {
-      // alert('Failed to get FCM token' + error)
-    }
-  );
-};
-
 export default {
   mounted() {
+    FirebasePlugin.grantPermission(function(hasPermission) {
+      console.log("Permission was " + (hasPermission ? "granted" : "denied"));
+    });
+
     var comp = this;
-    // this.getToken()
+    this.getToken();
+    if (cordova.platformId === "ios") {
+      this.getApnToken();
+    }
+    this.platformID = cordova.platformId;
     // this.onApnsTokenReceived()
   },
   methods: {
@@ -168,7 +103,7 @@ export default {
       var comp = this;
       FirebasePlugin.getAPNSToken(
         function(token) {
-          alert(token);
+          // alert(token);
           comp.apnToken = token;
         },
         function(error) {
@@ -181,7 +116,8 @@ export default {
     return {
       fcmToken: "",
       apnToken: "",
-      isHidden: false
+      isHidden: true,
+      platformID: ""
     };
   }
 };
